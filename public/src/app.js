c414318678209bd5995fa3f8e5006de4950296a4
Sonali Dhalwar <92299962+SonaliDhalwar@users.noreply.github.com>
2021-10-14 19:13:59 +0530

    Update app.js

--- public/src/app.js
+++ public/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import IndecisionApp from './components/IndecisionApp';
-import ReactCoures from './Components/ReactChapters';
+import ReactChapters from './Components/ReactChapters';
 //import CounterApp from './components/CounterApp';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
